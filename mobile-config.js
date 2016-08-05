2ae9a050afcbd2f9ba49aa302dad58cd9da4d38a
John Pinkerton <jpinkerton88@gmail.com>
2016-08-05 15:07:10 -0400

    add mobile platforms

---
+++ mobile-config.js
@@ -0,0 +1,10 @@
+App.info({
+  id: 'cc.newspring.LaunchCrosswalkExample',
+  name: 'LaunchCrosswalkExample',
+  description: 'Example of using launch with Crosswalk',
+  author: 'NewSpring Church',
+  email: 'web@newspring.cc',
+  website: 'https://newspring.cc',
+  version: '0.0.1',
+  buildNumber: '1'
+});
